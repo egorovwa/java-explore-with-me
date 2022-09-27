a65d6e4769b5cee4ee7066158e1822b713826f1f
vasiliy <egorovwa2@gmail.com>
2022-09-27 10:42:10 +0700

    add ewm-statistContract

---
+++ ewm-stats-contract/src/test/java/ru/practicum/ewmstatscontract/EwmStatsContractApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.practicum.ewmstatscontract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwmStatsContractApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
